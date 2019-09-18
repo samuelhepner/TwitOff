e52c632d23ca493c15adc88c36ac24b9b7f0f5a8
SamH3pn3r <samuelhepner03@gmail.com>
2019-09-18 14:06:57 -0700

    lecture day 3

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,22 +2,17 @@
 from flask import Flask, render_template, request
 from .models import DB, User
 from decouple import config
-from .twitter import pullUsersAndTweets
+from .twitter import add_or_update_user
 
 
 # make our own app factory
 def create_app():
     app = Flask(__name__)
-
-    # add config
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-
     # Stop tracking modifications
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    app.config['ENV'] = config('ENV')
-
+    # app.config['ENV'] = config('ENV')
     # now the database knows about the app
     DB.init_app(app)
 
@@ -28,11 +23,32 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
 
-    # create the route
-    @app.route('/getUsersAndTweets')
-    # define the function
-    def getUsersAndTweets():
-        pullUsersAndTweets('selenagomez')
-        return 'Twitter user(s) and tweets loaded!'
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
+
+    # # create the route
+    # @app.route('/getUsersAndTweets')
+    # # define the function
+    # def getUsersAndTweets():
+    #     pullUsersAndTweets('selenagomez')
+    #     return 'Twitter user(s) and tweets loaded!'
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset!', users=[])
 
     return app
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -14,17 +14,44 @@ TWITTER = tweepy.API(TWITTER_AUTH)
 BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
 
-def pullUsersAndTweets(user):
-    twitter_user = TWITTER.get_user(user)
-    tweets = twitter_user.timeline(count=200, include_rts=False,
-                                   tweet_mode='extended')
+# def pullUsersAndTweets(user):
+#     twitter_user = TWITTER.get_user(user)
+#     tweets = twitter_user.timeline(count=200, include_rts=False,
+#                                    tweet_mode='extended')
 
-    db_user = User(id=twitter_user.id, name=twitter_user.screen_name)
-    for tweet in tweets:
-        embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
-        db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500],
-                         embedding=embedding)
-        DB.session.add(db_tweet)
-        db_user.tweets.append(db_tweet)
-    DB.session.add(db_user)
-    DB.session.commit()
+#     db_user = User(id=twitter_user.id, name=twitter_user.screen_name)
+#     for tweet in tweets:
+#         embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+#         db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500],
+#                          embedding=embedding)
+#         DB.session.add(db_tweet)
+#         db_user.tweets.append(db_tweet)
+#     DB.session.add(db_user)
+#     DB.session.commit()
+
+
+def add_or_update_user(username):
+    # Add or update a user *and* their Tweets, error if no/private user.
+    try:
+        twitter_user = TWITTER.get_user(username)
+        db_user = (User.query.get(twitter_user.id) or
+                   User(id=twitter_user.id, name=username))
+        DB.session.add(db_user)
+        # We want as many recent non-retweet/reply statuses as we can get
+        tweets = twitter_user.timeline(
+                                        count=200,
+                                        exclude_replies=True,
+                                        include_rts=False,
+                                        tweet_mode='extended')
+        for tweet in tweets:
+            embedding = BASILICA.embed_sentence(tweet.full_text,
+                                                model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500],
+                             embedding=embedding)
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
+    except Exception as e:
+        print('Error processing {}: {}'.format(username, e))
+        raise e
+    else:
+        DB.session.commit()
